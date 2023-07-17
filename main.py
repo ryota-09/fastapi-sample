98b1812dcab89f69450d22ecf90d685dfc136c8a
ryota-09 <n.ryo950911@gmail.com>
2023-07-18 07:51:27 +0900

    databaseの基本設定

--- main.py
+++ main.py
@@ -26,4 +26,12 @@ async def index():
 
 @app.post("/users")
 async def users(users: User):
-  return { "user": users }
+  return { "users": users }
+
+@app.post("/bookings")
+async def bookings(bookings: Booking):
+  return { "bookings": bookings }
+
+@app.post("/rooms")
+async def rooms(rooms: Room):
+  return { "rooms": rooms }
